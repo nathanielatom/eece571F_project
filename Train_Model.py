8c763006e384cbdf97960209a59940ca4f5bf06d
Jason Fung <jasonfung@Jasons-MacBook-Air-2.local>
2022-03-17 16:54:46 -0700

    fixed shape inaccuracy

--- Train_Model.py
+++ Train_Model.py
@@ -17,8 +17,31 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.utils import to_categorical
 # from keras_preprocessing.image import ImageDataGenerator
 
+def crop_3D(img, new_size):
+    img_shape = img.shape
+    x_mid = int(img_shape[0]/2)
+    y_mid = int(img_shape[1]/2)
+    z_mid = int(img_shape[2]/2)
+
+    x_diff = int(abs(new_size[0]-x_mid))
+    y_diff = int(abs(new_size[1]-y_mid))
+    z_diff = int(abs(new_size[2]-z_mid))
+
+    x_start = x_mid-x_diff
+    y_start = y_mid-y_diff
+    z_start = z_mid-z_diff
+
+    tmp_img = img[x_start:x_start+new_size[0],y_start:y_start+new_size[1],z_start:z_start+new_size[2]]
+    return tmp_img
+
 scalar = MinMaxScaler()
-def generate_brats_batch(file_pattern, contrasts, batch_size=2, tumour='*', patient_ids='*', augment_size=None):
+def generate_brats_batch(file_pattern, 
+                         contrasts, 
+                         batch_size=32, 
+                         tumour='*', 
+                         patient_ids='*',
+                         crop_size = (None,None,None), 
+                         augment_size=None):
     """
     Generate arrays for each batch, for x (data) and y (labels), where the contrast is treated like a colour channel.
     
@@ -44,11 +67,15 @@ def generate_brats_batch(file_pattern, contrasts, batch_size=2, tumour='*', pati
     
     # get the shape of one 3D volume and initialize the batch lists
     arbitrary_contrast = contrasts[0]
-    shape = nib.load(filenames_by_contrast[arbitrary_contrast][0]).get_fdata().shape
+    if crop_size == (None,None,None):
+        shape = nib.load(filenames_by_contrast[arbitrary_contrast][0]).get_fdata().shape
+    else:
+        shape = crop_size
+
+    # initialize empty array of batches
     x_batch = np.empty((batch_size, ) + shape + (len(contrasts), )) #, dtype=np.int32)
     y_batch = np.empty((batch_size, ) + shape + (n_classes,)) #, dtype=np.int32)
     num_images = len(filenames_by_contrast[arbitrary_contrast])
-    print(num_images)
     np.random.shuffle(filenames_by_contrast[arbitrary_contrast])
     for bindex in tqdm.tqdm_notebook(range(0, num_images, batch_size), total=num_images):
         filenames = filenames_by_contrast[arbitrary_contrast][bindex:bindex + batch_size]
@@ -58,11 +85,12 @@ def generate_brats_batch(file_pattern, contrasts, batch_size=2, tumour='*', pati
                 # load raw image batches and normalize the pixels
                 tmp_img = nib.load(filename.replace(arbitrary_contrast, contrast)).get_fdata()
                 tmp_img = scalar.fit_transform(tmp_img.reshape(-1, tmp_img.shape[-1])).reshape(tmp_img.shape)
-                x_batch[findex, ..., cindex] = tmp_img
+                x_batch[findex, ..., cindex] = crop_3D(tmp_img, crop_size)
 
                 # load mask batches and change to categorical
                 tmp_mask = nib.load(filename.replace(arbitrary_contrast, 'seg')).get_fdata()
                 tmp_mask[tmp_mask==4] = 3
+                tmp_mask = crop_3D(tmp_mask, crop_size)
                 tmp_mask = to_categorical(tmp_mask, num_classes = 4)
                 y_batch[findex] = tmp_mask
         
@@ -78,8 +106,8 @@ def generate_brats_batch(file_pattern, contrasts, batch_size=2, tumour='*', pati
 
 
 tumours = ['LGG','HGG']
-# prefix = '/Users/jasonfung/Documents/EECE571' # Jason's Macbook
-prefix = 'C:/Users/Fungj/Documents/EECE_571F' # Jason's Desktop
+prefix = '/Users/jasonfung/Documents/EECE571' # Jason's Macbook
+# prefix = 'C:/Users/Fungj/Documents/EECE_571F' # Jason's Desktop
 brats_dir = '/MICCAI_BraTS_2018_Data_Training/'
 # prefix = '/home/atom/Documents/datasets/brats' # Adam's Station
 file_pattern = '{prefix}/MICCAI_BraTS_2018_Data_Training/{tumour}/{patient_id}/{patient_id}_{contrast}.nii.gz'
@@ -100,17 +128,18 @@ test_ratio = 0.2
 
 train_file, test_file = file_list_shuffled[0:int(len(file_list_shuffled)*(1-test_ratio))], file_list_shuffled[int(len(file_list_shuffled)*(1-test_ratio)):]
 
-train_datagen = generate_brats_batch(file_pattern, contrasts, batch_size = 2, patient_ids = train_file) # first iteration
-test_datagen = generate_brats_batch(file_pattern, contrasts, batch_size = 2,patient_ids = test_file) # first iteration
+batch_size = 2
+train_datagen = generate_brats_batch(file_pattern, contrasts, batch_size = batch_size, patient_ids = train_file , crop_size= (192,192,128)) # first iteration
+test_datagen = generate_brats_batch(file_pattern, contrasts, batch_size = batch_size, patient_ids = test_file, crop_size= (192,192,128)) # first iteration
 
 
 import segmentation_models_3D as sm 
 sm.set_framework('tf.keras')
 
 # data parameters
-x_size = None
-y_size = None
-z_size = None
+x_size = 192
+y_size = 192
+z_size = 128
 contrast_channels = 3
 input_shape = (x_size, y_size, z_size, contrast_channels)
 n_classes = 4
@@ -144,8 +173,15 @@ model = sm.Unet(backbone_name= BACKBONE,
 
 model.compile(optimizer = optim, loss = total_loss, metrics = metrics)
 
-with tf.device('/device:CPU:0'):
+steps_per_epoch = len(train_file)//batch_size
+val_steps_per_epoch = len(test_file)//batch_size
+
+
+with tf.device('/device:GPU:0'):
 	history = model.fit(train_datagen,
-	                    epochs = 50,
+	                    steps_per_epoch = steps_per_epoch,
 	                    verbose = 1,
-	                    validation_data = test_datagen)
+	                    validation_data = test_datagen,
+                        validation_steps = val_steps_per_epoch)
+
+
